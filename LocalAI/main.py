bb370ea2b431f35aa7df02c13bb46c53da87ae00
hugokk12 <hugokohmann@hotmail.com>
2025-12-28 17:17:46 -0300

    Local AI V2

--- LocalAI/main.py
+++ LocalAI/main.py
@@ -1,6 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from vector import retriever
+from vector import *
 
 model = OllamaLLM(model="llama3.2")
 
@@ -13,6 +13,16 @@ Here is the question to answer: {question}
 """
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
+
+VectorRetriever_instance = VectorRetriever()
+VectorRetriever_instance.read_csv(
+    csv_path            ="LocalAI/realistic_restaurant_reviews.csv",
+    embeddings_model    ="mxbai-embed-large",
+    db_location         ="./LocalAI/chrome_langchain_db"
+)
+VectorRetriever_instance.create_vector_store()
+
+retriever = VectorRetriever_instance.retriever
 
 while True:
     print("\n\n-------------------------------")
--- LocalAI/vector.py
+++ LocalAI/vector.py
@@ -4,34 +4,43 @@ from langchain_core.documents import Document
 import os
 import pandas as pd
 
-df = pd.read_csv("LocalAI/realistic_restaurant_reviews.csv")
-embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 
-db_location = "./chrome_langchain_db"
-add_documents = not os.path.exists(db_location)
+class VectorRetriever:
+    def __init__(self):
+        pass
 
-if add_documents:
-    documents = []
-    ids = []
+    def read_csv(self, csv_path, embeddings_model, db_location):
+        df = pd.read_csv(csv_path)
+        self.db_location = db_location
+        self.embeddings = OllamaEmbeddings(model=embeddings_model)
+        self.add_documents = not os.path.exists(self.db_location)
+
+        if self.add_documents:
+            documents = []
+            ids = []
+            
+            for i, row in df.iterrows():
+                document = Document(
+                    page_content=row["Title"] + " " + row["Review"],
+                    metadata={"rating": row["Rating"], "date": row["Date"]},
+                    id=str(i)
+                )
+                ids.append(str(i))
+                documents.append(document)
+            self.documents = documents
+            self.ids = ids
     
-    for i, row in df.iterrows():
-        document = Document(
-            page_content=row["Title"] + " " + row["Review"],
-            metadata={"rating": row["Rating"], "date": row["Date"]},
-            id=str(i)
+    def create_vector_store(self):
+                
+        vector_store = Chroma(
+            collection_name="restaurant_reviews",
+            persist_directory=self.db_location,
+            embedding_function=self.embeddings
         )
-        ids.append(str(i))
-        documents.append(document)
-        
-vector_store = Chroma(
-    collection_name="restaurant_reviews",
-    persist_directory=db_location,
-    embedding_function=embeddings
-)
 
-if add_documents:
-    vector_store.add_documents(documents=documents, ids=ids)
-    
-retriever = vector_store.as_retriever(
-    search_kwargs={"k": 5}
-)
+        if self.add_documents:
+            vector_store.add_documents(documents=self.documents, ids=self.ids)
+            
+        self.retriever = vector_store.as_retriever(
+            search_kwargs={"k": 5}
+        )
